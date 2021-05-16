1c0e2ca0dfa6feb700f2065d0e41313ece4632e3
Richard <richardshuai98@gmail.com>
2021-05-15 19:25:06 -0700

    Update workflow to merge counts with dge table

--- workflow/scripts/normalize_TMM.R
+++ workflow/scripts/normalize_TMM.R
@@ -1,15 +1,16 @@
+log <- file(snakemake@log[[1]], open="wt")
+sink(log)
+sink(log, type="message")
+
 library("edgeR")
 
-cts <- read.table("results/counts/all.tsv", header=TRUE, row.names="gene", check.names=FALSE)
+cts <- read.table(snakemake@input[["counts"]], header=TRUE, row.names="gene", check.names=FALSE)
 
-coldata <- read.table("config/samples.tsv", header=TRUE, row.names="sample_name", check.names=FALSE)
+coldata <- read.table(snakemake@params[["samples"]], header=TRUE, row.names="sample_name", check.names=FALSE)
 
 group <- as.factor(coldata$condition)
 dge <- DGEList(counts=cts, group=group)
-
-# keep <- filterByExpr(dge)
-# dge <- dge[keep, keep.lib.sizes=FALSE]
 dge <- calcNormFactors(dge, method='TMM')
 tmm <- cpm(dge)
 
-write.table(tmm, file='results/counts/TMM_normalized.tsv', quote=FALSE, sep='\t', col.names = NA)
+write.table(tmm, file=snakemake@output[["table"]], quote=FALSE, sep='\t', col.names = NA)
